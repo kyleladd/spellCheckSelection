3d7c4611ebda4606c9c7aca3acdb2152b3436cb7
Kyle Ladd <kladd6@gmail.com>
2016-04-09 20:12:37 -0400

    Convert selection into popup modal textbox for Grammarly to check

--- content_script.js
+++ content_script.js
@@ -1,7 +1,9 @@
 var checkKeyStr;
 //Get the selected text and send request to background
 function spellcheckSelection() {
+  console.log("spell check selection");
   var focused = document.activeElement;
+  console.log("focused", focused);
   var selectedText;
   if (focused) {
     try {
@@ -15,6 +17,33 @@ function spellcheckSelection() {
     var selectedText = sel.toString();
   }
   chrome.extension.sendRequest({'inputString': selectedText});
+  console.log("append the element");
+  var body = document.getElementsByTagName("body")[0];
+  var input = document.createElement("textarea");
+  // input.cols = "80";
+  // input.rows = "40";
+  var modal = document.createElement("div");
+  var modal_content_container = document.createElement("div");
+  var modal_close = document.createElement("div");
+  modal.classList.add("cms__modal");
+  modal_content_container.classList.add("cms__modal_content");
+  modal_close.classList.add("cms__close");
+  modal_close.innerHTML = "Close";
+  input.value = selectedText;
+  body.appendChild(modal); //appendChild
+  modal.appendChild(modal_content_container);
+  modal.appendChild(modal_close);
+  modal_content_container.appendChild(input);
+  input.focus();
+  console.log("element appended");
+  // document.getElementsByClassName("cms__close").onclick = function() {
+  document.getElementsByClassName("cms__close")[0].addEventListener("click", function(){
+    console.log("Clicked Close");
+    body.removeChild(modal);
+  });
+  //   console.log("Clicked Close");
+  //   body.removeChild(modal);
+  // }
 }
 
 function onExtensionMessage(request) {
@@ -37,6 +66,8 @@ function initContentScript() {
     if (!document.hasFocus()) {
       return true;
     }
+    // console.log("cmon");
+
     var keyStr = keyEventToString(evt);
     if (keyStr == checkKeyStr && checkKeyStr.length > 0) {
       spellcheckSelection();
@@ -47,5 +78,4 @@ function initContentScript() {
     return true;
   }, false);
 }
-
 initContentScript();
